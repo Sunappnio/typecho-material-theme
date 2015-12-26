bfe789c56e6b528498502dd8d00b70c56fc0fff6
Mr. Sun <Sunappnio@users.noreply.github.com>
2015-12-26 13:37:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $this->need('header.php');
 			        	<span>分类：<?php $this->category(','); ?> | </span>
 			        	<span>评论：<a href="<?php $this->permalink() ?>"><?php $this->commentsNum('%d 评论'); ?></a> </span>
 			        </div>
-			        <div class="post-content"><?php $this->content('-阅读全文-'); ?></div>
+			        <div class="post-content"><?php $this->content('- 阅读全文 -'); ?></div>
 			    </div>
 		    </div>
 		    <?php endwhile; ?>
